9cbb874b1312629e9ce8f86eb901fc6c1ab73ac3
Lou Franco <lfranco@greenwave-solutions.com>
2017-01-18 23:19:53 -0500

    Implement the GameplayKit version

--- PuzOMatPlayground.playground/Contents.swift
+++ PuzOMatPlayground.playground/Contents.swift
@@ -4,14 +4,18 @@ import UIKit
 
 /// This is a mock button we'll use to hold our game state. In a real game, 
 /// this would likely be a collection of components.
-struct Button {
+class Button: NSObject {
     let tapCount: Int
+
+    init(tapCount: Int) {
+        self.tapCount = tapCount
+    }
 }
 
 /// These are the facts that can be asserted about our game. If no fact is asserted,
 /// the game allows the user to keep playing. When they win, it moves on to the next
 /// level. If they hit a reset, the buttons all go back to an initial state.
-enum GameOutcome {
+enum GameOutcome: NSString {
     case win
     case reset
 }
@@ -46,6 +50,7 @@ evaluateLevel001(buttons: [Button(tapCount:0), Button(tapCount:1)])
 /// In this puzzle, you need to tap each button in order.
 func evaluateLevel002(buttons: [Button]) -> GameOutcome? {
     var foundUntappedButton = false
+
     for button in buttons {
         if button.tapCount >= 2 {
             return .reset
@@ -58,9 +63,11 @@ func evaluateLevel002(buttons: [Button]) -> GameOutcome? {
             return .reset
         }
     }
+
     if !foundUntappedButton {
         return .win
     }
+
     return nil
 }
 
@@ -105,7 +112,7 @@ typealias Rule = (_: [Button]) -> GameOutcome?
 /// - returns: a Rule that resets the game if the player taps any button maxTaps times.
 func makeTapLimitRule(maxTaps: Int) -> Rule {
     return { buttons in
-        if (buttons.filter { $0.tapCount >= maxTaps }).count > 0 {
+        if (buttons.first { $0.tapCount >= maxTaps }) != nil {
             return .reset
         }
         return nil
@@ -115,7 +122,7 @@ func makeTapLimitRule(maxTaps: Int) -> Rule {
 /// - returns: a Rule that results in a win if the player taps each button once
 func makeAllTappedRule() -> Rule {
     return { buttons in
-        if (buttons.filter { $0.tapCount == 1 }).count == buttons.count {
+        if (buttons.first { $0.tapCount != 1 }) == nil {
             return .win
         }
         return nil
@@ -158,3 +165,62 @@ func evaluateLevel001WithRules(buttons: [Button]) -> GameOutcome? {
 evaluateLevel001WithRules(buttons: [Button(tapCount:2), Button(tapCount:1)])
 evaluateLevel001WithRules(buttons: [Button(tapCount:1), Button(tapCount:1)])
 evaluateLevel001WithRules(buttons: [Button(tapCount:0), Button(tapCount:1)])
+
+/// Reimplement using GameplayKit
+import GameplayKit
+
+/// Basic usage of GKRuleSystem
+func evaluate(state: [String: Any], rules: [GKRule]) -> GameOutcome? {
+    let rs = GKRuleSystem()
+    rs.add(rules)
+    rs.state.addEntries(from: state)
+    rs.evaluate()
+
+    if rs.facts.count > 0, let fact = rs.facts[0] as? NSString {
+        return  GameOutcome(rawValue: fact)
+    }
+    return nil
+}
+
+/// Using GKRule's init that takes blocks
+func blockRulesForLevel001() -> [GKRule] {
+    return [
+        GKRule(blockPredicate: { rs in
+            guard let buttons = rs.state["b"] as? [Button] else { return false }
+            return (buttons.first { $0.tapCount >= 2 }) != nil
+        }, action: { rs in
+            rs.assertFact(GameOutcome.reset.rawValue)
+        }),
+        GKRule(blockPredicate: { rs in
+            guard let buttons = rs.state["b"] as? [Button] else { return false }
+            return (buttons.first { $0.tapCount != 1 }) == nil
+        }, action: { rs in
+            rs.assertFact(GameOutcome.win.rawValue)
+        }),
+    ]
+}
+
+evaluate(state: ["b": [Button(tapCount: 2), Button(tapCount: 1)]], rules: blockRulesForLevel001())
+evaluate(state: ["b": [Button(tapCount: 1), Button(tapCount: 1)]], rules: blockRulesForLevel001())
+evaluate(state: ["b": [Button(tapCount: 0), Button(tapCount: 1)]], rules: blockRulesForLevel001())
+
+/// Add a function to GameOutcome to make GKRule construction easier
+extension GameOutcome {
+    func assertIf(_ predicateFormat: String) -> GKRule {
+        return GKRule(predicate: NSPredicate(format: predicateFormat), assertingFact: self.rawValue, grade: 1.0)
+    }
+}
+
+/// The GKRules using our GameOutcome predicate extension
+func predicateRulesForLevel001() -> [GKRule] {
+    return [
+        GameOutcome.reset.assertIf("ANY $b.tapCount == 2"),
+        GameOutcome.win.assertIf("ALL $b.tapCount == 1"),
+    ]
+}
+
+
+evaluate(state: ["b": [Button(tapCount: 2), Button(tapCount: 1)]], rules: predicateRulesForLevel001())
+evaluate(state: ["b": [Button(tapCount: 1), Button(tapCount: 1)]], rules: predicateRulesForLevel001())
+evaluate(state: ["b": [Button(tapCount: 0), Button(tapCount: 1)]], rules: predicateRulesForLevel001())
+
